688752dae812ae93ad7c687a31e15d993ec0ba48
Salif Diarra <salif@live.ca>
2020-09-01 19:10:27 -0400

    FAIT : Programmer la validation du mot de passe

--- app/src/main/java/com/example/salifdiarra_lab1/MainActivity.java
+++ app/src/main/java/com/example/salifdiarra_lab1/MainActivity.java
@@ -2,11 +2,15 @@ package com.example.salifdiarra_lab1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -41,4 +45,22 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public void onClickBtn_valider(View view) {
+        EditText et_motDePasse = (EditText)findViewById(R.id.et_motDePasse);
+        TextView tv_validation =  (TextView)findViewById(R.id.tv_validation);
+        String regexMotDePasse = "^(?=.{10,})" + //Au moins 10 caractères
+                                 "(?=.*[a-z])" + //Au moins 1 minuscule
+                                 "(?=.*[A-Z])" + //Au moins 1 MAJUSCULE
+                                 "(?=.*[0-9])" + //Au moins 1 chiffre
+                                 //Au moins un caractère spécial autorisé
+                                 "(?=.*[@#$%&()\\[\\]{}_=<>+\\-!?*/|:;.,‘\"~^]).*$";
+
+        if (Pattern.matches(regexMotDePasse, et_motDePasse.getText())) {
+            tv_validation.setText(R.string.texteMotDePasseValide);
+            tv_validation.setTextColor(getResources().getColor(R.color.couleurMotDePasseValide));
+        } else {
+            tv_validation.setText(R.string.texteMotDePasseInvalide);
+            tv_validation.setTextColor(getResources().getColor(R.color.couleurMotDePasseInvalide));
+        }
+    }
 }
